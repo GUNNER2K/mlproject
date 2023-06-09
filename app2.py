bcde44222beca9768468eb81a88a296c1fc6c50f
Satyaki <satyakibhattacharjee252@gmail.com>
2023-06-10 02:27:44 +0530

    Postman API app

---
+++ app2.py
@@ -0,0 +1,19 @@
+import pickle
+from flask import Flask , request ,render_template , jsonify
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData , PredictPipeline
+
+app = Flask(__name__)
+
+@app.route('/predict' , methods = ['POST'])
+def predict():
+    json_ = request.json 
+    dataframe = pd.DataFrame(json_)
+    predict_pipeline = PredictPipeline()
+    results = predict_pipeline.predict(dataframe)
+    return str(results).split(' ')
+if __name__ == '__main__':
+    app.run(debug=True , port=5000)
